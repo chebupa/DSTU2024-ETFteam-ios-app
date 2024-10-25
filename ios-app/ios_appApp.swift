a6a1a072aa456f189d67f25a7e01939f15405aa1
chebupa <aristarhsakulin@gmail.com>
2024-10-25 18:28:01 +0300

    Initial Commit

---
+++ ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by aristarh on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
